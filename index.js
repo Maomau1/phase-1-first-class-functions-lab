22081868b43e6f85cbde3b8a47b525c9584bc713
maurice diong <mdiong00@gmail.com>
2025-05-26 01:54:59 -0400

    fucntions and callback functions lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array){
+    return array.slice(0,2)
+}
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = x => (y=>x*y)
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (arr,drivers){
+    return drivers(arr)
+}
